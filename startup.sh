a3a1337320ae3f1810244220a00d885dcfae8f53
Yanderuri <huanmai101@gmail.com>
2023-11-16 23:50:25 -0500

    added tmux plugins and and fixed oh-my-posh file

--- startup.sh
+++ startup.sh
@@ -1,1 +1,9 @@
-sudo apt install -y $(which packages.txt)
+sudo apt update
+sudo apt upgrade -y
+sudo apt install --no-install-suggests --install-suggests -y $(which packages.txt)
+chsh -s $(which zsh)
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+echo "Remember to add your flatpak store extension:\nGNOME: apt install gnome-software-plugin-flatpak\nKDE: apt install plasma-discover-backend-flatpak"
+
+sudo flatpak install flathub com.usebottles.bottles
