b9f199f5b77f77ababc86740f005b1b086de644c
Yanderuri <50468506+Yanderuri@users.noreply.github.com>
2023-11-17 23:13:40 -0500

    fixed a goofy ahh mistake with apt install added checking for root

--- startup.sh
+++ startup.sh
@@ -1,10 +1,19 @@
-sudo apt update
-sudo apt upgrade -y
-sudo apt install --no-install-suggests --install-suggests -y $(which packages.txt)
+ROOTUID="0"
+if [ "$(id -u)" -ne "$ROOTUID" ] ; then
+    echo "This script must be executed with root privileges."
+    exit 1
+fi
+# https://stackoverflow.com/questions/6362428/how-do-i-determine-if-a-shell-script-is-running-with-root-permissions
+
+apt update
+apt upgrade -y
+apt install --no-install-suggests --install-recommends -y $(cat packages.txt)
 chsh -s $(which zsh)
-sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 echo "Remember to add your flatpak store extension:\nGNOME: apt install gnome-software-plugin-flatpak\nKDE: apt install plasma-discover-backend-flatpak"
 
-sudo flatpak install -y flathub com.usebottles.bottles 
-ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+flatpak install -y flathub com.usebottles.bottles
+flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
+
+exit 0
