6675eb6b5f506b6be04c1073ad08a99c20b05b3c
Yanderuri <huanmai101@gmail.com>
2023-11-18 22:22:18 -0500

    added error checking and copying zshrc over

--- startup.sh
+++ startup.sh
@@ -8,12 +8,26 @@ fi
 apt update
 apt upgrade -y
 apt install --no-install-suggests --install-recommends -y $(cat apps/packages.txt)
-chsh -s $(which zsh)
+cp themes/.zshrc ~/.zshrc
+chsh -s $(command -v zsh)
 
 echo "Remember to add your flatpak store extension:\nGNOME: apt install gnome-software-plugin-flatpak\nKDE: apt install plasma-discover-backend-flatpak"
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub com.usebottles.bottles
 flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
+
+# https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
+
+if !command -v curl &> /dev/null
+then
+	echo "installing curl"
+	apt install -y --no-install-suggests --install-recommends curl
+fi
+# 2023-11-18: Hardcoded releases might not work forever
+# But that is a later Vy problem
+curl https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/AnonymousPro.zip -o AnonymousePro.zip
+
+
 
 exit 0
