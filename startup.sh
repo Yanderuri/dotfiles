2a0688f2efb4b2e7982caf7028dcbc85fc45c278
Yanderuri <huanmai101@gmail.com>
2023-11-22 23:07:11 -0500

    added flatpaks loop

--- startup.sh
+++ startup.sh
@@ -14,20 +14,18 @@ chsh -s $(command -v zsh)
 echo "Remember to add your flatpak store extension:\nGNOME: apt install gnome-software-plugin-flatpak\nKDE: apt install plasma-discover-backend-flatpak"
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install -y flathub com.usebottles.bottles
-flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
+while IFS= read -r flatpak; do
+  # Install the Flatpak
+  flatpak install -y flathub "$flatpak"
+done < "apps/flatpaks.txt"
 
+exit 0
 # https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
 
-if !command -v curl &> /dev/null
-then
-	echo "installing curl"
-	apt install -y --no-install-suggests --install-recommends curl
-fi
 # 2023-11-18: Hardcoded releases might not work forever
 # But that is a later Vy problem
 curl https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/AnonymousPro.zip -o AnonymousePro.zip
-
-
+unzip AnonymousePro.zip -d ~/.local/share/fonts
+fc-cache -fv
 
 exit 0
